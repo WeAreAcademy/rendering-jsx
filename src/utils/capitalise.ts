4e968f492cfc1fdcdbfba0b606fef2418eba5a93
Richard Ng <richardcrng@gmail.com>
2021-06-13 12:07:07 +0100

    Add documentation for capitalise function

--- src/utils/capitalise.ts
+++ src/utils/capitalise.ts
@@ -1,3 +1,8 @@
+/**
+ * Capitalises the first letter *only* of a string (and preserves the rest).
+ * @param str string to capitalise
+ * @returns the capitalised string
+ */
 export function capitalise(str: string): string {
   const capitalisedFirstLetter = str[0].toUpperCase();
   const remainingLetters = str.slice(1);
